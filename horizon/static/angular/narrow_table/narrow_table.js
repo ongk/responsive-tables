d0eafc87928279b0ead5f342343c77419dc226dd
Kelly Domico <kelly.domico@hp.com>
2014-12-15 15:46:18 -0800

    Added hover/select backgrounds, adjust table overflow based on action column size, added check all

--- horizon/static/angular/narrow_table/narrow_table.js
+++ horizon/static/angular/narrow_table/narrow_table.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
     lastWidth = $(window).width();
   });
 
+  var initActionColumn = function() {
+    var actionColWidth = $(".action-table tbody tr:first-child td:last-child").width();
+    $(".action-table").css("padding-right", actionColWidth+14);
+    $(".action-table thead tr th:last-child").width(actionColWidth);
+  };
+
   $(".expand-table").on("click", ".fa-chevron-right, .fa-chevron-down", function(e) {
     var icon = $(this);
     var summaryRow = icon.closest("tr");
@@ -24,6 +30,25 @@ $(document).ready(function() {
     }
   });
 
+  $(".action-table th.multi-select-col input:checkbox").on("click", function() {
+    var isChecked = $(this).is(":checked");
+    if (isChecked) {
+      $(".multi-select-col").closest("tr").addClass("row-selected").find("input:checkbox").attr("checked", "checked");
+    } else {
+      $(".multi-select-col").closest("tr").removeClass("row-selected").find("input:checkbox").removeAttr("checked");
+    }
+  });
+
+  $(".action-table td.multi-select-col input:checkbox").on("click", function() {
+    var isChecked = $(this).is(":checked");
+    if (isChecked) {
+      $(this).closest("tr").addClass("row-selected");
+    } else {
+      $(this).closest("tr").removeClass("row-selected");
+      $(".action-table th.multi-select-col input:checkbox").removeAttr("checked");
+    }
+  });
+
   $(window).resize(function() {
     var curWidth = $(window).width();
     if (lastWidth !== curWidth) {
@@ -35,5 +60,7 @@ $(document).ready(function() {
       });
     }
   });
+
+  initActionColumn();
 
 });
